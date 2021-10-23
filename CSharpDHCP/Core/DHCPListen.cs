a1fb23afe217d351923d93cca522c7434077c590
fursion <fursion@fursion.cn>
2021-10-23 10:43:15 +0800

    update

---
+++ CSharpDHCP/Core/DHCPListen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+namespace CSharpDHCP.Core
+{
+    public class DHCPListen
+    {
+        UdpClient _LISTEN = new UdpClient();
+        public DHCPListen()
+        {
+            
+
+        }
+    }
+}
+
